ad348ad5ef0e4a88819bdf81b29d919b89352e40
1jna1 <2944309620@qq.com>
2025-10-27 16:49:16 +0800

    Delete Desktop/MyCode/51 directory

--- Desktop/MyCode/51/AT24C02/AT24C02.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AT24C02_H__
-#define __AT24C02_H__
-
-void AT24C02_WriteData(unsigned char Reg_Address,unsigned char Data);
-void AT24C02_PageWriteData(unsigned char Reg_Address,unsigned char *Data);
-unsigned char AT24C02_ReadData(unsigned char Reg_Address);
-
-#endif
