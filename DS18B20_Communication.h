43bfe0f340be13b4195156341f3b15423259e38a
1jna1 <2944309620@qq.com>
2025-10-27 15:49:57 +0800

    Delete DS18B20_Communication.h

--- DS18B20_Communication.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DS18B20_COMMUNICATION_H__
-#define __DS18B20_COMMUNICATION_H__
-
-void DS18B20_MasterWrite_1(void);
-void DS18B20_MasterWrite_0(void);
-void DS18B20_Master_Tx_Byte(unsigned char Byte);
-
-#endif
